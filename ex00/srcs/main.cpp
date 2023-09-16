5cbea09c409666c9d9b3fe722c013d10387d967e
dorakueYaritai <kotto555555@gmail.com>
2023-09-16 13:33:21 +0900

    00

---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,54 @@
+#include "whatever.hpp"
+#include <iostream>
+
+int pdfTest()
+{
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
+
+int Test(const char *a, const char *b)
+{
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return (0);
+}
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    std::cout << T::value_type << std::endl;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+int main()
+{
+    pdfTest();
+    std::string a = "AAA";
+    std::string b = "ZZZ";
+    Test(a.c_str(), b.c_str());
+
+    return (0);
+}
---
+++ ex00/srcs/whatever.hpp
@@ -0,0 +1,30 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T   min(const T &a, const T &b)
+{
+    if (b <= a) // if a equal b, return b
+        return (b);
+    else
+        return (a);
+}
+
+template <typename T>
+T   max(const T &a, const T &b)
+{
+    if (b >= a) // if a equal b, return b
+        return (b);
+    else
+        return (a);
+}
+
+#endif
